6683064a1c88eead7300567b36cd18beae7ee764
miumiujiang12138 <129245663+miumiujiang12138@users.noreply.github.com>
2024-09-19 15:48:42 +0800

    add settings.py and update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,20 @@
 import sys
 import pygame
+from settings import Settings
 def run_game():
     # 初始化游戏并且创建一个屏幕对象
-    pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    pygame.init()    #初始化背景设置
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))  # 创建一个screen窗口(screen对象)
     pygame.display.set_caption("Alien Invasion")
     #开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
+        for event in pygame.event.get():       #监测事件函数    
             if event.type == pygame.QUIT:
                 sys.exit()
-        # 让最近绘制的屏幕可见
+        # 每次循环都重绘屏幕
+        screen.fill(ai_settings.bg_color)
+        # 让最近绘制的屏幕可见,刷新屏幕显示
         pygame.display.flip()
 run_game()
