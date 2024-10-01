f338b12e5e6da4dcfd5bfc432b62411db09adea1
miumiujiang12138 <129245663+miumiujiang12138@users.noreply.github.com>
2024-10-01 14:29:27 +0800

    accelerate the ship speed

--- ship.py
+++ ship.py
@@ -1,7 +1,9 @@
 import pygame
 class Ship():
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """初始化飞船图像并设置其初始位置"""
+        
+        self.ai_settings = ai_settings
         self.screen = screen
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('ship.bmp') # 返回一个surface对象，在Pygame中，Surface对象用于
@@ -12,14 +14,16 @@ class Ship():
         # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)    #将位置信息存储在浮点数变量中
         self.moving_right = False
         self.moving_left = False
     def update(self):
         """根据移动标志调整飞船的位置"""
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center         # 更新完self.center后再更新self.rect.centerx
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image,self.rect)
