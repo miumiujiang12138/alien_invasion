af93cece9b4647ccbf16809b21da10321078fc71
miumiujiang12138 <129245663+miumiujiang12138@users.noreply.github.com>
2024-10-19 17:09:16 +0800

    make aliens trun right move

--- settings.py
+++ settings.py
@@ -14,3 +14,6 @@ class Settings():
         self.bullet_color = 60,60,60
         self.bullet_allowed = 3
         self.alien_speed_factor =1
+        self.fleet_drop_speed =10
+        # fleet_direction =1表示向右移动，-1表示向左移动
+        self.fleet_direction =1
