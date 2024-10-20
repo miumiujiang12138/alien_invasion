146ca83c52646814cb6784e9ed7e3f992ead6be3
miumiujiang12138 <129245663+miumiujiang12138@users.noreply.github.com>
2024-10-20 16:24:07 +0800

    ship detect collisions with aliens

--- game_functions.py
+++ game_functions.py
@@ -113,7 +113,11 @@ def change_fleet_direction(ai_settings,aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def update_aliens(ai_seetings,aliens):
+def update_aliens(ai_seetings,ship,aliens):
     """更新外星人群中所有外星人的位置"""
     check_fleet_edges(ai_seetings,aliens)
     aliens.update()
+    # 检测外星人和飞船之间的碰撞
+    if pygame.sprite.spritecollideany(ship,aliens):
+        print("Ship hit!!!")
+
